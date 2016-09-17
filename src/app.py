77fd1d6d875c4012d57864632eeb07b100802f70
Soqueen <sokhenglim973@gmail.com>
2016-09-17 15:43:08 +0700

    generate form for user input

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,11 +9,20 @@ app = Flask(__name__)
 def home():
     return render_template("base.html")
 
+
 @app.route("/create")
 def create():
     return render_template("create.html")
 
+
+@app.route('/save_data', methods=('GET', 'POST'))
+def save_data():
+    if request.method == 'POST':
+        # placeholder for database
+        # Grab info to store somewhere
+        return render_template("event.html")
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
-
