b30a62e96577918fbc1f85cdf850c12c8a559f0d
xTEddie <t.eddie@live.com>
2016-09-18 06:51:38 -0400

    Add ability to store planner data in db

--- src/app.py
+++ src/app.py
@@ -10,9 +10,9 @@ HOSTNAME = "www.eventplanner.com"
 ref_id = None
 
 
-# def generate_unique_url():
-#     """ Generator Unique URL."""
-#     return str(uuid.uuid4())[:11].replace("-", "").lower()
+def generate_unique_url():
+    """ Generator Unique URL."""
+    return str(uuid.uuid4())[:11].replace("-", "").lower()
 
 
 @app.route("/")
@@ -76,22 +76,23 @@ def share():
 def planner(ref_id):
     if request.method == 'POST':
         # Retrieve data from DB here
-        guest_name = request.form["name"]
-        attending = request.form["attending"] #
-        insert_guest(
-            guest_name,
-            attending,
-            ref_id
-        )
-
+        pass
     return render_template("planner.html", ref_id=ref_id)
 
 
 @app.route('/planner/<ref_id>/save', methods=('GET', 'POST'))
 def save_planner(ref_id):
     if request.method == 'POST':
-        # Save guests data in DB here
-        return "YES"
+        guest_name = request.form["name"]
+        attending = request.form.getlist("attending")
+
+        insert_guest(
+            guest_name,
+            True if attending else False,
+            ref_id
+        )
+
+        return "YES" # Should return to another page
 
 
 @app.route('/split_budget', methods=('GET', 'POST'))
