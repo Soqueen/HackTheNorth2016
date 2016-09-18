17975d199fa2de2c73b4243001e385f9bfa2be16
xTEddie <t.eddie@live.com>
2016-09-18 02:48:12 -0400

    Add data manipulation of the event data into the database

--- src/app.py
+++ src/app.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import sys
+import uuid
 from flask import Flask, render_template, request
+from api.database import insert_event
 
 app = Flask(__name__)
 
 
+def generate_unique_url():
+    """ Generator Unique URL."""
+    return str(uuid.uuid4())[:11].replace("-", "").lower()
+
+
 @app.route("/")
 def home():
+    # print("HI", file=sys.stderr)
     return render_template("base.html")
 
 
@@ -23,8 +32,27 @@ def create():
 @app.route('/save_general', methods=('GET', 'POST'))
 def save_data():
     if request.method == 'POST':
-        # placeholder for database
-        # Grab info to store somewhere
+        event_name = request.form["eventname"]
+        event_location = request.form["eventlocation"]
+        event_date = request.form["event_date"]
+        event_start_time = request.form["eventstarttime"]
+        event_end_time = request.form["eventendtime"]
+        event_description = request.form["eventdescription"]
+        event_host_name = request.form["username"]
+        host_email = request.form["user_email"]
+
+        url_ref = generate_unique_url()
+
+        insert_event(
+            event_name,
+            "{} {}".format(event_date, event_start_time),
+            event_location,
+            event_description,
+            event_host_name,
+            host_email,
+            url_ref
+        )
+
         return render_template("invitation.html")
     return render_template("create.html")
 
